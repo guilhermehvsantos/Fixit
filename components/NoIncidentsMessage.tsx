2bc067c6849c89172eb58a24f641db237518ace8
GuilhermeHVSantos <guilhermehvs@outlook.com>
2025-04-22 17:51:09 -0300

    Add self assign functionality

--- components/NoIncidentsMessage.tsx
+++ components/NoIncidentsMessage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use client";
 
 interface NoIncidentsMessageProps {
@@ -13,3 +14,20 @@ export function NoIncidentsMessage({ isRegularUser }: NoIncidentsMessageProps) {
     </div>
   );
 }
+=======
+"use client";
+
+interface NoIncidentsMessageProps {
+  isRegularUser: boolean;
+}
+
+export function NoIncidentsMessage({ isRegularUser }: NoIncidentsMessageProps) {
+  return (
+    <div className="flex flex-col items-center justify-center py-12">
+      <h3 className="mt-4 text-lg font-medium text-navy-900">
+        Nenhum chamado encontrado
+      </h3>
+    </div>
+  );
+}
+>>>>>>> fd4214c (Add self assign functionality)
