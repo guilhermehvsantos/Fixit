a8a5e2697dc6c74fc27748104758f21193e655c6
Guilherme Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2025-04-22 19:48:55 -0300

    Update NoIncidentsMessage.tsx

--- components/NoIncidentsMessage.tsx
+++ components/NoIncidentsMessage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 interface NoIncidentsMessageProps {
@@ -14,20 +13,3 @@ export function NoIncidentsMessage({ isRegularUser }: NoIncidentsMessageProps) {
     </div>
   );
 }
-=======
-"use client";
-
-interface NoIncidentsMessageProps {
-  isRegularUser: boolean;
-}
-
-export function NoIncidentsMessage({ isRegularUser }: NoIncidentsMessageProps) {
-  return (
-    <div className="flex flex-col items-center justify-center py-12">
-      <h3 className="mt-4 text-lg font-medium text-navy-900">
-        Nenhum chamado encontrado
-      </h3>
-    </div>
-  );
-}
->>>>>>> fd4214c (Add self assign functionality)
